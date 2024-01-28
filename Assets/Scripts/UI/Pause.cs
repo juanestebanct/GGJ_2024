0444bd4deb3cd29fdee7bd99533641b8a5c1138a
jaydeeay <jarroyavep@outlook.com>
2024-01-28 14:13:41 -0500

    Utpade

--- Assets/Scripts/UI/Pause.cs
+++ Assets/Scripts/UI/Pause.cs
@@ -2,14 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Pause : MonoBehaviour
 {
     [SerializeField] private GameObject _settingsMenu;
+    [SerializeField] CanvasGroup FadeBackground;
+
     private bool paused;
 
     private void Start()
     {
+        FadeBackground.alpha = 0;
         InputManager.Instance.OnPausePressed += TogglePause;
         paused = false;
         _settingsMenu.SetActive(false);
@@ -19,17 +23,33 @@ public class Pause : MonoBehaviour
     {
         if (!paused)
         {
+            FadeBackground.gameObject.SetActive(true);
             Time.timeScale = 0;
-            _settingsMenu.SetActive(true);
             Cursor.lockState = CursorLockMode.None;
+            FadeBackground.DOFade(1, 1.5f).SetUpdate(true);
+
+
+
         }
         else
         {
             Time.timeScale = 1;
-            _settingsMenu.SetActive(false);
+
             Cursor.lockState = CursorLockMode.Locked;
+
+            FadeBackground.gameObject.SetActive(true);           
         }
 
         paused = !paused;
     }
+
+    public void OpenOptions()
+    {
+        _settingsMenu.SetActive(false);
+    }
+    public void OpenOptions()
+    {
+        _settingsMenu.SetActive(true);
+    }
+
 }
