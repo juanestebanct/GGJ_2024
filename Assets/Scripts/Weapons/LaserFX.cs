6dcf592bc6932bf10f010853c68c7c6ebbd69317
Jose Daniel Montoya Molina <66889936+panchuel@users.noreply.github.com>
2024-01-28 13:47:01 -0500

    Musiquita y laser

--- Assets/Scripts/Weapons/LaserFX.cs
+++ Assets/Scripts/Weapons/LaserFX.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using AK;
+using AK.Wwise;
 
 public class LaserFX : MonoBehaviour
 {
@@ -10,6 +12,9 @@ public class LaserFX : MonoBehaviour
     [SerializeField] private Vector3 _laserScale = new Vector3(0.02f, 0.02f, 0.02f);
     [SerializeField] private float _laserStartTime;
     [SerializeField] private float _laserEndTime;
+
+    [SerializeField] private AK.Wwise.Event _laserSFX;
+
 
     public void SetHitParticlePosition(Vector3 pos)
     {
@@ -23,12 +28,16 @@ public class LaserFX : MonoBehaviour
         _laser.transform.localScale = Vector3.zero;
         DOTween.To(() => _laser.transform.localScale, x => _laser.transform.localScale = x,
             _laserScale, _laserStartTime);
+
+        _laserSFX.Post(gameObject);
     }
 
     public void EndLaser()
     {
         DOTween.To(() => _laser.transform.localScale, x => _laser.transform.localScale = x,
             new Vector3(0,0,_laserScale.z), _laserEndTime).onComplete = TurnOffLaser;
+
+        _laserSFX.Stop(gameObject);
     }
     
     private void TurnOffLaser(){ _laser.SetActive(false); }
