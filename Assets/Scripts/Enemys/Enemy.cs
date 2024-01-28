6dd687c9d89df1e679e9def92f01c93f4057ea35
Dr. 27 <dr27draws@gmail.com>
2024-01-28 15:06:55 -0500

    Kilometro

--- Assets/Scripts/Enemys/Enemy.cs
+++ Assets/Scripts/Enemys/Enemy.cs
@@ -1,11 +1,7 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using Unity.AI.Navigation;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public enum MoventPatron { Change, Attacking }
 public class Enemy : MonoBehaviour
@@ -71,7 +67,7 @@ public class Enemy : MonoBehaviour
     {
         print("collisiono algo?");
 
-            // se coloca el daño del laser 
+            // se coloca el daï¿½o del laser 
             RecieveDamage(1);
 
     }
